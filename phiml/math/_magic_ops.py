fdbb78e25894a7636ce83f48ec0f7de7eaf0b23a
eliasdjo <elias.gabriel.djossou@gmail.com>
2023-12-09 14:53:55 +0100

    fix phiml tensor comparison and unstacking of fields with norm / tang extrapol

--- phiml/math/_magic_ops.py
+++ phiml/math/_magic_ops.py
@@ -92,7 +92,8 @@ def unstack(value, dim: DimFilter) -> tuple:
                 assert all([isinstance(item, Sliceable) for item in result]), f"__unstack__ must return a tuple of Sliceable objects but not all items were sliceable in {result}"
                 return result
         # return tuple([slice_(value, {dims.name: i}) for i in range(dims.size)])
-        return tuple([slice_(value, {dims.name: i}) for i in dims.item_names[0]])
+        dim_description = dims.item_names if dims.item_names[0] is not None else range(dims.size)
+        return tuple([slice_(value, {dims.name: i}) for i in dim_description])
     else:  # multiple dimensions
         if hasattr(value, '__pack_dims__'):
             packed_dim = batch('_unstack')
